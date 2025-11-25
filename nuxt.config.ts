84c30b7a38f44e33f385e09a42efcd44ab069e41
Leandro Sani <leandrocs.sani@gmail.com>
2025-11-24 22:04:09 -0300

    adicionando uma fonte diferente

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,17 +5,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/image', '@nuxt/scripts', '@nuxt/ui'],
   css: ['~/assets/css/main.css'],
 
-  // ADICIONE ISSO AQUI:
-  /*app: {
-    // Troque 'quintas-dumonte' pelo NOME EXATO do seu repositório no GitHub
-    // Mantenha as barras no começo e no fim.
-    baseURL: '/quintas-dumonte/',
-    cdnURL: '/quintas-dumonte/',
-    buildAssetsDir: 'assets', // Opcional, ajuda a organizar
-  },
-*/
-
-    nitro: {
+  nitro: {
     preset: 'static'
   },
   
